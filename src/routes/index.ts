f6f9fbbb8937073c701aca62a0b9ccb85781aefd
Emilie G <emilie@neo-nomade.com>
2020-12-10 10:36:38 +0100

    fix error

--- src/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import secured from './secured';
-
-const routes = Router();
-routes.use('/', secured);
-
-export default routes;
--- src/routes/secured/index.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-import user from './user';
-
-const routes = Router();
-routes.use('/user', user);
-export default routes;
